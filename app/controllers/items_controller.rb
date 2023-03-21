be98ddfc80f28266f8845e65733b8218676d1137
Gregory <elvismabisi@gmail.com>
2023-03-21 22:39:37 +0300

    completed

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,9 @@
 class ItemsController < ApplicationController
+    def index
+        items = Item.all
+        render json: items.to_json(
+          only: [:id, :name, :description, :price],
+          include: { user: { only: [:id, :username, :city] } }
+        )
+      end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,9 @@
 class UsersController < ApplicationController
+    def show
+        user = User.find(params[:id])
+        render json: user.to_json(
+            only: [:id, :username, :city],
+            include: {items: {only: [:id, :name, :description, :price]}}
+        )
+    end    
 end
